5f33e6d193749d9c7ac3d385a1346dd7fd8c73d8
Crazymind <crazymind2004@gmail.com>
2012-07-09 11:49:40 -0400

    file upload added

--- routes/home.js
+++ routes/home.js
@@ -1,5 +1,5 @@
 module.exports = function(app) {
-
+    var fs = require('fs');
     // home page
     app.get('/', function(req, res) {        
         res.render('index', { title: 'Home Page.  ' })
@@ -17,5 +17,24 @@ module.exports = function(app) {
     // about page
     app.get('/demo', function(req, res) {
         res.render('demo', { title: 'Demo page.  ' })
-    });   
+    });  
+    app.get('/upload_image', function(req, res) {
+        res.render('upload', { title: 'Upload images here.  ' })
+    });
+    
+    app.post('/upload_image', function(req, res) {
+        fs.readFile(req.files.displayImage.path, function(err, data) {
+            var newPath = __dirname + "../../uploads/" + req.files.displayImage.name;
+            var newItem = fs.writeFile(newPath, data, function(err) {
+                res.write('\n upload failed!');
+                res.redirect("back");
+            });
+
+            res.render('upload_thanks', {
+                title: 'thanks for upload',
+                imagedata: newItem
+            });
+
+        });
+    });
 }
