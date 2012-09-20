ca198c18006d2e21dba6c7bf499601adfa10c2b4
Crazymind <crazymind2004@gmail.com>
2012-09-20 14:47:00 +0300

    backbone part update

--- public/javascripts/task.js
+++ public/javascripts/task.js
@@ -1,45 +1,55 @@
-var item_template = '<div class="view"><input class="toggle" type="checkbox" <%= done ? \'checked="checked"\' : \'\' %> /><label><%= title %></label><a class="destroy"></a></div><input class="edit" type="text" value="<%= title %>" />';
-var stats_template ='<% if (done) { %><a id="clear-completed">Clear <%= done %> completed <%= done == 1 ? \'item\' : \'items\' %></a><% } %><div class="todo-count"><b><%= remaining %></b> <%= remaining == 1 ? \'item\' : \'items\' %> left</div>';
+//var item_template = '<div class="view"><input class="toggle" type="checkbox" <%= done ? \'checked="checked"\' : \'\' %> /><label><%= title %></label><a class="destroy"></a></div><input class="edit" type="text" value="<%= title %>" />';
+//var stats_template ='<% if (done) { %><a id="clear-completed">Clear <%= done %> completed <%= done == 1 ? \'item\' : \'items\' %></a><% } %><div class="todo-count"><b><%= remaining %></b> <%= remaining == 1 ? \'item\' : \'items\' %> left</div>';
 //var my_Template = 'First name: <input id="first_name"><br/>Last Name: <input id="last_name"><br/><button id="save">Save!</button>';
-var my_Template = 'First name: <input id="first_name"><br/>Last Name: <input id="last_name"><br/><button id="save">Save!</button>';
+//var my_Template = 'First name: <input id="first_name"><br/>Last Name: <input id="last_name"><br/><button id="save">Save!</button>';
 
 $(document).ready(function(){
 
-	MyModel = Backbone.Model.extend({
+	TasksModel = Backbone.Model.extend({
 	  urlRoot: "/task_source",
-	  id : 123,
 	  //id : new Date().getTime(),
 	  silent : true
 	});
 
-	MyCollection = Backbone.Collection.extend({
-		model: MyModel,
+	tasksCollection = Backbone.Collection.extend({
+		model: TasksModel,
 		url : '/task_source',
 		initialize: function() {
-
+			this.name = "MyCollection";
 		}
 	});
 
-	var Inst = new MyCollection();
+	var Inst = new tasksCollection();
 
-	MyView = Backbone.View.extend({
+	TaskGenerator = Backbone.View.extend({
 		template: _.template($('#my_template').html()),
-
+		el: '<div class="app"></div>',
 		events: {
-			"change #first_name": "setFirstName",
+			//"change #first_name": "setFirstName",
+			"change .item": "itemEditComplete",
+			"change .duration": "calcCost",
 			//"hover .item":"setFirstName1",
-			"click .edit": "changeTtl",
+			//"click .edit": "changeTtl",
+			//"keydown .cost, .eta, .duration": "checkNumber",
+			//"dblclick .title": "changeTtl",
+			"click .eta": "showDatapicker",
+			"dblclick .item": "changeTtl",
+			//"blur .item input": "itemEditComplete",
 			"click .save": "save",
-			"change .title": "itemEditComplete",
-			"change #last_name":"setLastName",
-			"click #save": "save"
+			"click .submit-task": "submitModel"
+
 		},
 
 		initialize: function(){
 			var _this = this;
-			_.bindAll(this, "saveSuccess", "saveError");
+			this.name = "TaskGenerator";
 
-			var data = Inst.fetch({
+			_.bindAll(this, "saveSuccess", "saveError", "reset");
+			Inst.bind('reset', this.reset); // binds method to the collection .fetch sucess callback
+			Inst.bind('error', this.fetchError);
+			Inst.fetch();
+			
+			/*var data = Inst.fetch({
 				error: function(model, response) {
 					console.log('fetch error');
 				},
@@ -47,20 +57,53 @@ $(document).ready(function(){
 					_this.model.items = response;
 					myView.render();
 				}
-			});
-
+			});*/
 			//console.log('args: ', [].splice.call(arguments,0));
 		},
-
-		setFirstName: function(e){
+		fetchError: function(model, response) {
+			console.log('fetch error ', response);
+		},
+		reset: function(){ // fetch callback / don't know if rename
+			this.model.items = Inst.toJSON()[0];
+			this.render();
+		},
+		render: function(){
+			var items = this.model.items;
+			this.itemsCollection = $();
+			if(!items) return;
+			var html;
+			//$('<div class="new-item-wrapper"><input type="text" /></div>').insertBefore($(this.el));
+			$(this.el).append(_.template($('#task_header_template').html()));
+			for(var _i in items){
+				var item_html = $(this.template(items[_i]));
+				item_html.data('saveBtn', item_html.find('button.save'));
+				this.itemsCollection = this.itemsCollection.add(item_html);
+				$(this.el).append(item_html);
+			}
+			$(this.el).append(_.template($('#submit-btm-tpl').html()));
+			return this;
+		},
+		checkNumber: function(e){
+			if(!e.currentTarget.firstChild.length <= 0) return;
+			var reg = /([0-9])/gi;
+			var val = $(e.currentTarget).children().val().match(reg);
+			if(val) val = val.join('')*1;
+			$(e.currentTarget).children().val(val ? val : 0)
+		},
+		calcCost: function calcCost(e){
+			$(e.currentTarget).closest('.view').find('.cost').text($(e.currentTarget).find('input').val()*15);		
+		},
+		setItem: function(e){
 			var val = $(e.currentTarget).val();
 			this.model.set({first_name: val});
 		},
-		setFirstName1: function(e){
-			console.log('click');
+		showDatapicker: function(e){
+			e.stopImmediatePropagation();
+			console.log('show calendar');
 		},
 		changeTtl: function(e){
-			var el = $(e.currentTarget).closest('.view').find('.title');
+			var el = this.itemsCollection.find($(e.currentTarget));
+			if(el.hasClass('eta') || el.hasClass('edit') || el.hasClass('buttons')) return;
 			var val = el.text();
 			el.empty();
 			var edit = el.find('input');
@@ -75,56 +118,58 @@ $(document).ready(function(){
 			//this.model.set({first_name: val});
 		},
 		itemEditComplete: function(e){
-			console.log(e);
 			var el = $(e.currentTarget);
+			if(el && el.hasClass('cost') || el.hasClass('eta') || el.hasClass('duration')) this.checkNumber(e);
 			var val = $(e.target).val();
 			var edit = $('<div class="ttl-holder">'+val+'</div>');
 			el.append(edit);
 			$(e.target).hide();
 			//this.model.set({first_name: val});
 		},
-
-
 		setLastName: function(e){
 			var val = $(e.currentTarget).val();
 			this.model.set({last_name: val});
 		},
 		parseData : function (el) {
-			
-			var data = {
-				"task1": {
-					qweqweqwe: 'qweqeqweqweqwe'
-				}
-			}
-			console.log(data.task1);
+			var data = {};
+			var num = el.find('.id').val();
+			var arr = ["title", "duration","cost","eta","link","done"];
+			data["task"+ num] = {
+				"id": num
+			};
+			for (var i = arr.length - 1; i >= 0; i--) {
+				data["task"+ num][arr[i]] = el.find('.'+arr[i]).text();
+			};
 			return data;
 		},
+		submitModel: function submitModel(e){
+			console.log(this.model);
+			console.log(this.model.toJSON());
+			this.model.save();
+		},
 		save: function(e){
+			console.log('save');
 			e.preventDefault(); // prevent the button click from posting back to the server
-			this.model.save(this.parseData($(e.currentTarget).closest('.view')), {success: this.saveSuccess, error: this.saveError});
+			console.log(this.parseData($(e.currentTarget).closest('.view')));
+			this.model.set(this.parseData($(e.currentTarget).closest('.view')));
+			//this.model.save(this.parseData($(e.currentTarget).closest('.view')), {success: this.saveSuccess, error: this.saveError});
+			console.log('md: ',this.model.toJSON());
 		},
 
 		saveSuccess: function(model, response){
 			// do things here after a successful save to the server
+			console.log('save callback');
+			//console.log(model);
+			//$(myView.el).empty();
+			//Inst.fetch();
 		},
 
 		saveError: function(model, response){
-			// do things here after a failed save to the server
-		},
-		render: function(){
-			console.log('render', this.model.items);
-
-			var html;
-			for(var _i in this.model.items){
-				var item_html = this.template(this.model.items[_i]);
-				$(this.el).append($(item_html));
-			}
-			return this;
+			console.log('smth went ololo!11');
 		}
-
 	});
 
-	myModel = new MyModel();
-	myView = new MyView({model: myModel});
-	$("#todoapp").html(myView.el);
+	tasksModel = new TasksModel();
+	generateTaskTable = new TaskGenerator({model: tasksModel});
+	$("#todoapp").html(generateTaskTable.el);
 });
--- routes/home.js
+++ routes/home.js
@@ -1,126 +1,136 @@
 module.exports = function(app)
 {
-    fs = require('fs');
+	fs = require('fs');
 
-    nconf = require('nconf'),
-    Recaptcha = require('recaptcha').Recaptcha;
+	nconf = require('nconf'),
+	Recaptcha = require('recaptcha').Recaptcha;
 
-    // home page
-    app.get('/', function(req, res)
-    {
-        res.render('index', { title: 'Home Page.  ' })
-    });
+	// home page
+	app.get('/', function(req, res)
+	{
+		res.render('index', { title: 'Home Page.  ' })
+	});
 
-    // chat area
-    app.get('/chat', function(req, res)
-    {
-        res.render('chat', { title: 'Chat with Me!  ' })
-    });
+	// chat area
+	app.get('/chat', function(req, res)
+	{
+		res.render('chat', { title: 'Chat with Me!  ' })
+	});
 
-    // about page
-    app.get('/about', function(req, res)
-    {
-        res.render('about', { title: 'About Me.  ' })
-    });
-    // about page
-    app.get('/demo', function(req, res)
-    {
-        res.render('demo', { title: 'Demo page.  ' })
-    });
-    // task page
-    app.get('/task', function(req, res)
-    {
-        res.render('task', { title: 'task page.  ' })
-    });
-    var dataOld = {
-        "glossary": {
-            "title": "example glossary",
-            "GlossDiv": {
-                "title": "S",
-                "GlossList": {
-                    "GlossEntry": {
-                        "ID": "SGML",
-                        "SortAs": "SGML",
-                        "GlossTerm": "Standard Generalized Markup Language",
-                        "Acronym": "SGML",
-                        "Abbrev": "ISO 8879:1986",
-                        "GlossDef": {
-                            "para": "A meta-markup language, used to create markup languages such as DocBook.",
-                            "GlossSeeAlso": ["GML", "XML"]
-                        },
-                        "GlossSee": "markup"
-                    }
-                }
-            }
-        }
-    }
-    var data = {"task0":{"title":"empty task from model","duration":0,"cost":0,"eta":"1/1/12","link":"http://google.com","done":false},"task1":{"title":"empty task1","duration":0,"cost":0,"eta":"1/1/12","link":"http://google.com","done":false},"task2":{"title":"empty task2","duration":0,"cost":0,"eta":"1/1/12","link":"http://google.com","done":false},"task3":{"title":"empty task3","duration":0,"cost":0,"eta":"1/1/12","link":"http://google.com","done":false},"task4":{"title":"empty task4","duration":0,"cost":0,"eta":"1/1/12","link":"http://google.com","done":false}};
-    var dataSep = {
-        "task0": {
-            "title": "empty task from model",
-            "id" : 0,
-            "duration": 0,
-            "cost": 0,
-            "eta": "1/1/12",
-            "link": "http://google.com",
-            "done": false
-        },
-        "task1": {
-            "title": "empty task1",
-            "id" : 1,
-            "duration": 0,
-            "cost": 0,
-            "eta": "1/1/12",
-            "link": "http://google.com",
-            "done": false
-        },
-        "task2": {
-            "title": "empty task2",
-            "id" : 2,
-            "duration": 0,
-            "cost": 0,
-            "eta": "1/1/12",
-            "link": "http://google.com",
-            "done": false
-        },
-        "task3": {
-            "title": "empty task3",
-            "id" : 3,
-            "duration": 0,
-            "cost": 0,
-            "eta": "1/1/12",
-            "link": "http://google.com",
-            "done": false
-        },
-        "task4": {
-            "title": "empty task4",
-            "id" : 4,
-            "duration": 0,
-            "cost": 0,
-            "eta": "1/1/12",
-            "link": "http://google.com",
-            "done": false
-        }
-    }
-    
-    app.get('/task_source', function(req, res)
-    {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.write(JSON.stringify(dataSep));
-        res.end();
-    });
-    app.post('/task_source', function(req, res)
-    {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        //if(req.body) data = req.body;
-        res.write(JSON.stringify(dataSep));
-        res.end();
-    });
-    app.put('/task_source/123', function(req, res)
-    {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        //if(req.body) data = req.body;
-        res.write(JSON.stringify(dataSep));
-        res.end();
-    });
+	// about page
+	app.get('/about', function(req, res)
+	{
+		res.render('about', { title: 'About Me.  ' })
+	});
+	// about page
+	app.get('/demo', function(req, res)
+	{
+		res.render('demo', { title: 'Demo page.  ' })
+	});
+	// task page
+	app.get('/task', function(req, res)
+	{
+		res.render('task', { title: 'task page.  ' })
+	});
+	var dataOld = {
+		"glossary": {
+			"title": "example glossary",
+			"GlossDiv": {
+				"title": "S",
+				"GlossList": {
+					"GlossEntry": {
+						"ID": "SGML",
+						"SortAs": "SGML",
+						"GlossTerm": "Standard Generalized Markup Language",
+						"Acronym": "SGML",
+						"Abbrev": "ISO 8879:1986",
+						"GlossDef": {
+							"para": "A meta-markup language, used to create markup languages such as DocBook.",
+							"GlossSeeAlso": ["GML", "XML"]
+						},
+						"GlossSee": "markup"
+					}
+				}
+			}
+		}
+	}
+	var data = {"task0":{"title":"empty task from model","duration":0,"cost":0,"eta":"1/1/12","link":"http://google.com","done":false},"task1":{"title":"empty task1","duration":0,"cost":0,"eta":"1/1/12","link":"http://google.com","done":false},"task2":{"title":"empty task2","duration":0,"cost":0,"eta":"1/1/12","link":"http://google.com","done":false},"task3":{"title":"empty task3","duration":0,"cost":0,"eta":"1/1/12","link":"http://google.com","done":false},"task4":{"title":"empty task4","duration":0,"cost":0,"eta":"1/1/12","link":"http://google.com","done":false}};
+	var dataSep = {
+		"task0": {
+			"title": "empty task from model",
+			"id" : 0,
+			"duration": 0,
+			"cost": 0,
+			"eta": "1/1/12",
+			"link": "http://google.com",
+			"done": false
+		},
+		"task1": {
+			"title": "empty task1",
+			"id" : 1,
+			"duration": 0,
+			"cost": 0,
+			"eta": "1/1/12",
+			"link": "http://google.com",
+			"done": false
+		},
+		"task2": {
+			"title": "empty task2",
+			"id" : 2,
+			"duration": 0,
+			"cost": 0,
+			"eta": "1/1/12",
+			"link": "http://google.com",
+			"done": false
+		},
+		"task3": {
+			"title": "empty task3",
+			"id" : 3,
+			"duration": 0,
+			"cost": 0,
+			"eta": "1/1/12",
+			"link": "http://google.com",
+			"done": false
+		},
+		"task4": {
+			"title": "empty task4",
+			"id" : 4,
+			"duration": 0,
+			"cost": 0,
+			"eta": "1/1/12",
+			"link": "http://google.com",
+			"done": false
+		}
+	}
+	
+	var in_memory_data = dataSep;
+
+	function updateList(in_memory_data, newData){
+		for (var i in newData) {
+			in_memory_data[i] = newData[i];
+		};
+		return in_memory_data;
+	}
+
+	app.get('/task_source', function(req, res)
+	{
+		res.writeHead(200, { 'Content-Type': 'application/json' });
+		console.log(in_memory_data);
+		res.write(JSON.stringify(in_memory_data));
+		res.end();
+	});
+	app.post('/task_source', function(req, res)
+	{
+		res.writeHead(200, { 'Content-Type': 'application/json' });
+		if(req.body) res.write(JSON.stringify(updateList(in_memory_data, req.body))); 
+		else res.write('no data');
+		res.end();
+	});
+	app.put('/task_source/123', function(req, res)
+	{
+		res.writeHead(200, { 'Content-Type': 'application/json' });
+		//if(req.body) data = req.body;
+		res.write(JSON.stringify(dataSep));
+		res.end();
+	});
 }
